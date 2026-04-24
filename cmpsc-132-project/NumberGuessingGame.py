aac01fbde1f2d233f313f85d73c319fe970ff5f8
Jake-Levy14 <jzl415@psu.edu>
2026-04-24 14:43:54 -0400

    Guesser Added

--- cmpsc-132-project/NumberGuessingGame.py
+++ cmpsc-132-project/NumberGuessingGame.py
@@ -9,15 +9,35 @@ from py4godot.classes.Node2D import Node2D
 class NumberGuessingGame(Node2D):
 
 	def _ready(self) -> None:
-		self.label = self.get_node("../Label")
+		self.num_input = self.get_node("../CanvasLayer/Input")
+		self.guesses_remaining_label = self.get_node("../CanvasLayer/GuessesRemaining")
+		self.hint_label = self.get_node("../CanvasLayer/HintLabel")
+		
 		self.target = random.randint(0,100)
-		self.label.text = str(self.get_target)
-
-
+		self.guesses_remaining = 5
+		self.hint_label.text = str(self.target)\
+		
+		self.guesses_remaining_label.text = str(self.guesses_remaining)
+		
+		self.num_input.grab_focus()
+		self.num_input.value_changed.connect(self._on_input_changed)
+		
+		
 	def _process(self, delta:float) -> None:
 		pass
 	
 	@property
 	def get_target(self):
 		return self.target
-		
+
+	def _on_input_changed(self, new_value: float):
+		if not isinstance(new_value,float):
+			print("Invalid Input")
+		if new_value == self.target:
+			self.hint_label.text = "You Win"
+		elif new_value < self.target:
+			self.hint_label.text = "Higher"
+		elif new_value > self.target:
+			self.hint_label.text = "Lower"
+		self.guesses_remaining -= 1
+		self.guesses_remaining_label.text = str(self.guesses_remaining)
